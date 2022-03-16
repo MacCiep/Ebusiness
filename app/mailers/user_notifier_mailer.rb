c8799683c0a2b73746ff12b0c8caec9689381889
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-03-16 23:38:45 +0100

    Change function name

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,6 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-  def user_notification
-    send_signup_email(user)
+  def send_signup_email(user)
     @user = user
     mail( :to => @user.email,
           :subject => 'Thanks for signing up for our amazing app'
