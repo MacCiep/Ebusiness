5ce56b93f1538318d18f9abdc5cb9ef00c4863cd
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-03-16 23:49:40 +0100

    Final Touches

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_12_172348) do
+ActiveRecord::Schema.define(version: 2022_03_12_210213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discounts", force: :cascade do |t|
+    t.bigint "room_id"
+    t.decimal "price_drop"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_discounts_on_room_id"
+  end
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
@@ -71,6 +79,7 @@ ActiveRecord::Schema.define(version: 2022_03_12_172348) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "discounts", "rooms"
   add_foreign_key "offers", "users"
   add_foreign_key "reservations", "rooms"
   add_foreign_key "reservations", "users"
