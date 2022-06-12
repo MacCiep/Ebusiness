2bd3fb9c05e96b1f2b5895b1c81d16d888f7f8e1
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-06-12 17:10:46 +0200

    Additional fields for Reservations and DraftReservation serializers

--- app/serializers/draft_reservation_serializer.rb
+++ app/serializers/draft_reservation_serializer.rb
@@ -4,6 +4,18 @@ class DraftReservationSerializer
 
   belongs_to :room, serializer: RoomSerializer
 
+  attribute :number_of_days do |draft_reservation|
+    (draft_reservation.end_date - draft_reservation.start_date).to_i
+  end
+
+  attribute :price_per_day do |draft_reservation|
+    draft_reservation.room.price_per_day
+  end
+
+  attribute :room_name do |draft_reservation|
+    draft_reservation.room.name
+  end
+
   attribute :final_price do |draft_reservation|
     day_quantity = (draft_reservation.end_date - draft_reservation.start_date).to_i
     day_quantity * draft_reservation.room.price_per_day
--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -4,6 +4,22 @@ class ReservationSerializer
   belongs_to :room
   belongs_to :payment_type, serializer: PaymentTypeSerializer
 
+  attribute :number_of_days do |reservation|
+    (reservation.end_date -  reservation.start_date).to_i
+  end
+
+  attribute :price_per_day do |reservation|
+    reservation.room.price_per_day
+  end
+
+  attribute :payment_type do |reservation|
+    reservation.payment_type.name
+  end
+
+  attribute :room_name do |reservation|
+    reservation.room.name
+  end
+
   attribute :purchase_date do |reservation|
     reservation.created_at
   end
