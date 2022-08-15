98873a5764bd1d11d2f154323afac2fb406fff17
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-08-15 12:54:21 +0200

    Fix admin authentication

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,8 @@
 module Admin
   class AdminController < ApplicationController
     include ErrorsHandlers
-    before_action :authenticate_admin!
+    before_action :authenticate_user!
+    after_action :authenticate_admin!, only: [:authenticate_user!]
 
     def authenticate_admin!
       raise Admin::LoginErrors::NotAnAdmin unless current_user.admin?
